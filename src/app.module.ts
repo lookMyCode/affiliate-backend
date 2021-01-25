8dff9bfe491ef7a654f06bf520501f1c363f249a
Dmytro <dimasik22101993@gmail.com>
2021-01-25 22:01:00 +0100

    Heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AffiliateModule } from './affiliate/affiliate.module';
 
+const DB_PASS = 'zaq12WSX';
+const DB_NAME = 'affiliate';
+const DB_URL = `mongodb+srv://dima:${DB_PASS}@cluster0.vqb5y.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/affiliate', { useNewUrlParser: true }),
+    MongooseModule.forRoot(DB_URL, { useNewUrlParser: true }),
     AffiliateModule,
   ],
   controllers: [AppController],
