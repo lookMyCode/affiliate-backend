d2206ff9e63a323de2fe72f954806e5f5f5cec59
Dmytro <dimasik22101993@gmail.com>
2021-01-25 11:41:03 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT ? process.env.PORT : 3000;
   app.enableCors();
-  await app.listen(PORT);
+  await app.listen(55680);
 }
 bootstrap();
